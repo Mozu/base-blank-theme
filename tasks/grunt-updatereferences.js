f38f03ae9a33c2c7fe41930180ad220ecdb640cf
james <zetlen@gmail.com>
2014-09-13 07:57:23 -0500

    fixing args

--- tasks/grunt-updatereferences.js
+++ tasks/grunt-updatereferences.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt) {
   grunt.registerTask('updatereferences', 'Update references folder with core theme and any other dependent themes', function() {
     var done = this.async();
-    grunt.util.spawn({ cmd: 'bower cache clean' }, function(err) {
+    grunt.util.spawn({ cmd: 'bower', args: ['cache','clean'] }, function(err) {
       if (err) grunt.fail.warn('Cache clean failed: ' + err.message);
       ([4,5]).reduceRight(function(cb, ver) {
         return function() {
