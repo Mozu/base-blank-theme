ab4ff1ed9920320157ba1532d8c9cdf060de03f4
james <zetlen@gmail.com>
2014-09-13 07:56:24 -0500

    typo

--- tasks/grunt-updatereferences.js
+++ tasks/grunt-updatereferences.js
@@ -2,14 +2,14 @@
 
 module.exports = function(grunt) {
   grunt.registerTask('updatereferences', 'Update references folder with core theme and any other dependent themes', function() {
-    var done = this.async(),
-
+    var done = this.async();
     grunt.util.spawn({ cmd: 'bower cache clean' }, function(err) {
       if (err) grunt.fail.warn('Cache clean failed: ' + err.message);
       ([4,5]).reduceRight(function(cb, ver) {
         return function() {
           grunt.util.spawn({
-            cmd: 'bower install core' + ver + '=mozu/core-theme#^' + ver + ' --production --config.directory="references"'
+            cmd: 'bower',
+            args: ('install core' + ver + '=mozu/core-theme#^' + ver + ' --production --config.directory="references"').split(' ')
           }, cb);
         }
       }, done)();
