dac84c62823cbce9fd75bcbc2af4c51ce963a39f
james <zetlen@gmail.com>
2014-09-13 07:51:54 -0500

    testing an approach that uses childprocess instead of direct bower api access

--- tasks/grunt-updatereferences.js
+++ tasks/grunt-updatereferences.js
@@ -3,15 +3,14 @@
 module.exports = function(grunt) {
   grunt.registerTask('updatereferences', 'Update references folder with core theme and any other dependent themes', function() {
     var done = this.async(),
-    bower = require('bower');
-    bower.commands.cache.clean().on('end', function() {
+
+    grunt.util.spawn({ cmd: 'bower cache clean' }, function(err) {
+      if (err) grunt.fail.warn('Cache clean failed: ' + err.message);
       ([4,5]).reduceRight(function(cb, ver) {
         return function() {
-          var j = bower.commands.install(['core' + ver + '=mozu/core-theme#^' + ver], { production: true }, { directory: 'references' });
-          j.on('log', function(arg) {
-            grunt.log.ok(arg.message);
-          });
-          j.on('end', cb);
+          grunt.util.spawn({
+            cmd: 'bower install core' + ver + '=mozu/core-theme#^' + ver + ' --production --config.directory="references"'
+          }, cb);
         }
       }, done)();
     });
