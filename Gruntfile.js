1a2572a66dc247832700dfe1e41bfd6cc8336ed7
james <zetlen@gmail.com>
2014-11-12 15:56:32 -0600

    small updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,7 @@ module.exports = function(grunt) {
     'README.md',
     'theme.json',
     'theme-ui.json',
+    '*.ico',
     '*.png'
   ],
 
--- configure.js
+++ configure.js
@@ -1,15 +1,22 @@
-var childProcess = require('child_process');
+var childProcess = require('child_process'),
+  windows = /^win/.test(process.platform);
 (function(cmds, done) {
   return cmds.reduceRight(function(cb, command) {
     return function() {
       console.log('\n>> ' + command.msg + "...");
       // the below platform sniff courtesy of https://github.com/joyent/node/issues/2318
-      if (/^win/.test(process.platform)) command.cmd = 'cmd /c ' + command.cmd;
+      if (windows) command.cmd = 'cmd /c ' + command.cmd;
       var cmd = command.cmd.split(' ');
       var p = childProcess.spawn(cmd[0], cmd.slice(1), { stdio: 'inherit' });
       p.on('close', function(code) {
         if (code !== 0) {
-          console.error('\n>> Command `' + cmd.join(' ') + '` exited with code ' + code + '. Aborting rest of configure.');
+          if (command.err) {
+            if (command.err(code) === true) {
+              cb();
+            }
+          } else {
+            console.error('\n>> Command `' + cmd.join(' ') + '` exited with code ' + code + '. Aborting rest of configure.');
+          }
         } else {
           cb();
         }
@@ -19,7 +26,12 @@ var childProcess = require('child_process');
 })([
   {
     msg: 'Installing global Grunt',
-    cmd: 'npm install -g grunt-cli'
+    cmd: 'npm install -g grunt-cli',
+    err: function(code) {
+      if (!windows) {
+        console.error('\n>> Installing global Grunt failed. Your directory permissions may be wrong. Try running this: \n\n\   sudo chown `whoami` `npm prefix -g` && sudo chown `whoami` `npm get cache`');
+      }
+    }
   },
   {
     msg: 'Installing global Bower',
