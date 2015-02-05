db91ed9f0ad56fdeef682322bf6de7afd2c16fe3
james <zetlen@gmail.com>
2015-02-05 11:10:34 -0600

    fixing bad bower version display

--- tasks/grunt-updatereferences.js
+++ tasks/grunt-updatereferences.js
@@ -15,7 +15,9 @@ module.exports = function(grunt) {
             args: ('install core' + ver + '=mozu/core-theme#^' + ver + ' -j --production --config.directory=references').split(' ')
           }, function(err, res) {
             if (err) grunt.fail.warn('Update references failed: ' + err.message || err);
-            JSON.parse(res.stderr).filter(function(log) { return log.id === "resolved" }).forEach(function(log) {
+            JSON.parse(res.stderr).filter(function(log) { 
+              return log.id === "resolved" && log.data && log.data.endpoint && log.data.endpoint.source === "mozu/core-theme"; 
+            }).forEach(function(log) {
               if (grunt.option('warn' + ver)) grunt.log.subhead("Core" + ver + " has changed in production. Check the release notes of the Core theme for changes, and re-test your theme.")
               grunt.log.ok("Your reference to Core" + ver + " has updated to version " + log.message.split('#').pop());
             });
